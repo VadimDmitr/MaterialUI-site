33127aa880ec751e1fd20c18e088416c07bdd7db
VadimDmitr <vadzzimluch@gmail.com>
2023-06-26 16:04:56 -0400

    Add iconbutton,button,styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import { AppBar } from "@mui/material";
-import { Container, Toolbar } from "@mui/material";
+import React from "react";
+import {
+  AppBar,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function App() {
   return (
-    <AppBar>
+    <AppBar position="fixed">
       <Container fixed>
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{
+              marginRight: 1, // theme.spacing(1)
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            Web Developer Blog
+          </Typography>
+          <Box mr={3}>
+            <Button color="inherit" variant="outlined">
+              {" "}
+              Log In{" "}
+            </Button>
+          </Box>
+          <Button color="secondary" variant="contained">
+            {" "}
+            Log Out{" "}
+          </Button>
+        </Toolbar>
       </Container>
     </AppBar>
   );
