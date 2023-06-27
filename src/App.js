4d75009d2b0644f8300155ea3c1fd739c008b6bd
VadimDmitr <vadzzimluch@gmail.com>
2023-06-26 23:00:40 -0400

    Add grid container

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ import {
   Button,
   Paper,
   Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useTheme } from "@mui/material/styles";
+import LayerIcon from "@mui/icons-material/Layers";
+import PlayCircleFilled from "@mui/icons-material/PlayCircleFilled";
+
+const cards = [1, 2, 3, 4, 5, 6];
 
 function App() {
   const theme = useTheme();
@@ -76,6 +84,7 @@ function App() {
                   background: "rgba(0,0,0, .9)",
                 }}
               />
+
               <Grid container>
                 <Grid item md={6}>
                   <div
@@ -101,7 +110,13 @@ function App() {
                       >
                         Web Developer Blog
                       </Typography>
-                      <Typography component="h5" variant="h6" color="inherit" gutterBottom paragraph>
+                      <Typography
+                        component="h5"
+                        variant="h6"
+                        color="inherit"
+                        gutterBottom
+                        paragraph
+                      >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Ut ullamcorper ut sem nec faucibus. Sed auctor augue et
                         neque commodo, a feugiat nisl fringilla. Phasellus quis
@@ -122,39 +137,100 @@ function App() {
               </Grid>
             </Container>
           </Paper>
-          <div /*className="mainContent"*/>
-            <Container maxWidth="md">
-              <Typography
-                variant="h2"
-                align="center"
-                color="textPrimary"
-                gutterBottom
-              >
-                Web Developer Blog
-              </Typography>
-              <Typography
-                variant="h5"
-                align="center"
-                color="textSecondary"
-                paragraph
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                ullamcorper ut sem nec faucibus. Sed auctor augue et neque
-                commodo, a feugiat nisl fringilla. Phasellus quis elit metus.
-              </Typography>
-              <div /*className="mainButtons"*/>
-                <Grid /*className="mainButtons"*/ container spacing={5} justifyContent="center">
-                  <Grid item>
-                    <Button variant="contained" color="primary">Start Now</Button>
-                    </Grid>
-                    <Grid item>
-                    <Button variant="outlined" color="primary">Learn More</Button>
-                  </Grid>
-                </Grid>
-              </div>
-            </Container>
-          </div>
         </Box>
+        <div /*className="mainContent"*/>
+          <Container maxWidth="md">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Web Developer Blog
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+              ullamcorper ut sem nec faucibus. Sed auctor augue et neque
+              commodo, a feugiat nisl fringilla. Phasellus quis elit metus.
+            </Typography>
+            <div /*className="mainButtons"*/>
+              <Grid
+                /*className="mainButtons"*/ container
+                spacing={5}
+                justifyContent="center"
+              >
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Start Now
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Learn More
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container
+          /* className="cardGrid"*/ maxWidth="md"
+          sx={{
+            marginTop: theme.spacing(4),
+          }}
+        >
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card /*className="card"*/
+                  sx={{
+                    backgroundColor: "#e9e9e9", // Set to the color of background
+                    borderRadius: "15px", // Set preference
+                    boxShadow:
+                      "20px 20px 60px #bebebe, -20px -20px 90px #ffffff",
+                      border: "1px solid #000000",
+                  }}
+                >
+                  <CardMedia /*class="cardMedia"*/
+                    image="https://source.unsplash.com/featured"
+                    title="Image title"
+                    sx={{
+                      paddingTop: "56.25%",
+                    }}
+                  />
+                </Card>
+                <CardContent /*className="cardContent"*/
+                  sx={{
+                    flexGrow: 1,
+                  }}
+                >
+                  <Typography variant="h5" gutterBottom>
+                    Blog Post
+                  </Typography>
+                  <Typography>
+                    Blog Post. Web Developer blog. Web Developer blog. Blog
+                    Post.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                  <LayerIcon />
+                  <PlayCircleFilled />
+                </CardActions>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
     </>
   );
